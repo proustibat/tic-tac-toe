bdd01833a523016200c735cf2edbc1d3375f5100
Proustibat <jennifer.proust@gmail.com>
2017-12-23 22:37:55 +0100

    refactor(Fonts): rename fonts files

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514064350337', dateBuild: '2017-12-23T21:25:50.337Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514065047015', dateBuild: '2017-12-23T21:37:27.015Z' };
