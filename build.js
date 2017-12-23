0167c9579d7ea7ac7c98d013c933e25d5f7912d4
Proustibat <jennifer.proust@gmail.com>
2017-12-23 22:26:24 +0100

    refactor(Node script): build

--- build.js
+++ build.js
@@ -3,87 +3,85 @@ const log   = console.log;
 const util  = require( 'util' );
 const fs    = require( 'fs' );
 
+const logger = {
+
+    title: val => {
+        const prefix = '|                                ';
+        const suffix = '                                 |';
+        const charTotal = prefix.length + val.length + suffix.length;
+        let underline = '|';
+        for ( let i = 0; i < charTotal - 2; i++ ) {
+            underline += '-';
+        }
+        underline += '|';
+        log( chalk.white.bgGreen.bold( `${ underline }` ) );
+        log( chalk.white.bgGreen.bold( `${ prefix }${ val }${ suffix }` ) );
+        log( chalk.white.bgGreen.bold( `${ underline }` ) );
+        log( '\r' );
+    },
+
+    separator: () => {
+        const charTotal = 84;
+        let underline = '';
+        for ( let i = 0; i < charTotal; i++ ) {
+            underline += '-';
+        }
+        log( chalk.green( `${ underline }` ) );
+    },
+
+    congrats: msg => {
+        log( '\r' );
+        log( chalk.white.bgGreen.bold( ` ☆    ${ util.inspect( msg.toUpperCase(), false, null ) }  ☆  ` ) );
+        log( '\r' );
+    },
+
+    info: msg => log( chalk.white( ( `►  ${ util.inspect( msg, false, null ) }` ) ) ),
+
+    done: msg => log( chalk.green( ( `✓  Done:\n ${ util.inspect( msg, false, null ) }` ) ) ),
+
+    error: err => log( chalk.red( (`☹  Error:\n ${ util.inspect( err, false, null ) }` ) ) )
+};
+
+class Build {
+
+    constructor() {
+        logger.title( 'PREPARE BUILD' );
+        logger.separator();
+        this.loadPackage();
+        this.prepareAppInfo();
+        this.writeAppInfo();
+    }
+
+    loadPackage() {
+        logger.info( 'Load ./package.json' );
+        this.pkg = require( './package.json' );
+        logger.done( 'package.json is loaded' );
+        logger.separator();
+    }
+
+    prepareAppInfo() {
+        logger.info( 'Prepare AppInfo data' );
+        const timestamp = Date.now();
+        this.contentFile = `window.AppInfo = { name: '${ this.pkg.name }', version: '${ this.pkg.version }', build: '${ timestamp }', dateBuild: '${ new Date( timestamp ).toISOString() }' };\n`;
+        logger.done( 'AppInfo ok' );
+        logger.separator();
+    }
+
+    writeAppInfo() {
+        logger.info( 'Writing content into file AppInfo.js' );
+        fs.writeFile( './src/js/AppInfo.js', `${ this.contentFile }`, err => {
+            if ( err ) {
+                logger.error( err );
+                return false;
+            }
+            logger.done( 'The file is saved!' );
+            logger.separator();
+            logger.congrats( 'MAZEL TOV!' );
+        });
+    }
+}
+
 module.exports = ( () => {
-
-    return new class Build {
-
-        constructor() {
-
-            this.log = {
-
-                title: val => {
-                    const prefix = '|                                ';
-                    const suffix = '                                 |';
-                    const charTotal = prefix.length + val.length + suffix.length;
-                    let underline = '|';
-                    for ( let i = 0; i < charTotal - 2; i++ ) {
-                        underline += '-';
-                    }
-                    underline += '|';
-                    log( chalk.white.bgGreen.bold( `${ underline }` ) );
-                    log( chalk.white.bgGreen.bold( `${ prefix }${ val }${ suffix }` ) );
-                    log( chalk.white.bgGreen.bold( `${ underline }` ) );
-                    log( '\r' );
-                },
-
-                separator: () => {
-                    const charTotal = 84;
-                    let underline = '';
-                    for ( let i = 0; i < charTotal; i++ ) {
-                        underline += '-';
-                    }
-                    log( chalk.green( `${ underline }` ) );
-                },
-
-                congrats: msg => {
-                    log( '\r' );
-                    log( chalk.white.bgGreen.bold( ` ☆    ${ util.inspect( msg.toUpperCase(), false, null ) }  ☆  ` ) );
-                    log( '\r' );
-                },
-
-                info: msg => log( chalk.white( ( `►  ${ util.inspect( msg, false, null ) }` ) ) ),
-
-                done: msg => log( chalk.green( ( `✓  Done:\n ${ util.inspect( msg, false, null ) }` ) ) ),
-
-                error: err => log( chalk.red( (`☹  Error:\n ${ util.inspect( err, false, null ) }` ) ) )
-            };
-
-            this.prepareBuild();
-        }
-
-        prepareBuild() {
-            this.log.title( 'PREPARE BUILD' );
-
-            this.log.separator();
-
-            this.log.info( 'Load ./package.json' );
-            const pkg = require( './package.json' );
-            this.log.done( 'package.json is loaded' );
-
-            this.log.separator();
-
-            this.log.info( 'Prepare AppInfo data' );
-            const timestamp = Date.now();
-            const contentFile = `window.AppInfo = { name: '${ pkg.name }', version: '${ pkg.version }', build: '${ timestamp }', dateBuild: '${ new Date( timestamp ).toISOString() }' };\n`;
-            this.log.done( 'AppInfo ok' );
-
-            this.log.separator();
-
-            this.log.info( 'Writing content into file AppInfo.js' );
-            fs.writeFile( './src/js/AppInfo.js', `${ contentFile }`, err => {
-                if ( err ) {
-                    this.log.error( err );
-                    return false;
-                }
-                this.log.done( 'The file is saved!' );
-
-                this.log.separator();
-
-                this.log.congrats( 'MAZEL TOV!' );
-            });
-        }
-
-    };
-
+    return new Build();
 } )();
 
--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514063863836', dateBuild: '2017-12-23T21:17:43.836Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514064350337', dateBuild: '2017-12-23T21:25:50.337Z' };
