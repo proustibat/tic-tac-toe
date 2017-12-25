a94f2ce31ea0162727e28cad79b3a7a847b7a35f
Proustibat <jennifer.proust@gmail.com>
2017-12-25 05:57:13 +0100

    fix(Favicon): Sonarqube issue
    https://sonarcloud.io/project/issues?id=prstbt.tictactoe&issues=AWCLbgysyrN5waWxeVdo&open=AWCLbgysyrN5waWxeVdo

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514176409847', dateBuild: '2017-12-25T04:33:29.847Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514177773017', dateBuild: '2017-12-25T04:56:13.017Z' };
