c8fa479a938983b858718ba5721e573d73cb3c02
Proustibat <jennifer.proust@gmail.com>
2017-12-27 17:18:04 +0100

    fix(Css lint):

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514386967739', dateBuild: '2017-12-27T15:02:47.739Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514391453158', dateBuild: '2017-12-27T16:17:33.158Z' };
