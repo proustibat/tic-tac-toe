cb947f135235a1834cbc01429888a9b3eed4550a
Proustibat <jennifer.proust@gmail.com>
2017-12-28 21:24:21 +0100

    fix(footer ui):

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514491022764', dateBuild: '2017-12-28T19:57:02.764Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514491554655', dateBuild: '2017-12-28T20:05:54.655Z' };
