7adc81ffe9908dd3e96455344f0390480ea8d2a5
Proustibat <jennifer.proust@gmail.com>
2017-12-29 14:19:24 +0100

    fix(Sonarcloud issues): iframe github: remove obsolete attributes
    https://sonarcloud.io/project/issues?id=prstbt.tictactoe&issues=AWCYwqtHyrN5waWxhISm&open=AWCYwqtHyrN5waWxhISm

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514491554655', dateBuild: '2017-12-28T20:05:54.655Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1514553243348', dateBuild: '2017-12-29T13:14:03.348Z' };
