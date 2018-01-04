b79949745efcb18e4f55157f403c0199f25994bf
Proustibat <jennifer.proust@gmail.com>
2018-01-04 22:30:12 +0100

    feat(Logger): Add a module logger to custom console

--- src/js/AppInfo.js
+++ src/js/AppInfo.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1515055853933', dateBuild: '2018-01-04T08:50:53.933Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1515091367899', dateBuild: '2018-01-04T18:42:47.899Z' };
