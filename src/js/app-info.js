1fe7322cd19c53397c0fa1c490eb836ef9502ae4
Proustibat <jennifer.proust@gmail.com>
2018-01-21 12:42:22 +0100

    docs(Update packages): following Greenkeeper

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.0', build: '1516036010264', dateBuild: '2018-01-15T17:06:50.264Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1516534892960', dateBuild: '2018-01-21T11:41:32.960Z' };
