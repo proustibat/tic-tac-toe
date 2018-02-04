987f5c7054f4ec142ed1d19f60e730b7d266ed94
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-04 16:42:45 +0100

    build(Dependency update): uglifyjs-webpack-plugin from 1.1.7 to 1.1.8
    1.1.7 had an issue

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1516534892960', dateBuild: '2018-01-21T11:41:32.960Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1517758891052', dateBuild: '2018-02-04T15:41:31.052Z' };
