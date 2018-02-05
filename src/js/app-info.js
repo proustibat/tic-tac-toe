392b01ad33c0aba907e90d0c0fa406029bc2b1cb
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-05 11:49:31 +0100

    chore(NPM): Update yarn.lock after eslint-plugin-node update

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1517758891052', dateBuild: '2018-02-04T15:41:31.052Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1517827725100', dateBuild: '2018-02-05T10:48:45.100Z' };
