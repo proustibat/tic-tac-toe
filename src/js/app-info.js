8e3e226572d7499fd3ecadc42981f12ebb78d718
Jennifer Proust <jennifer.proust@gmail.com>
2018-02-22 15:39:00 +0100

    build(Yarn.lock): updated after dependencies updates

--- src/js/app-info.js
+++ src/js/app-info.js
@@ -1,1 +1,1 @@
-window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1517827725100', dateBuild: '2018-02-05T10:48:45.100Z' };
+window.AppInfo = { name: 'prstbt-tictactoe', version: '1.0.2', build: '1519310281236', dateBuild: '2018-02-22T14:38:01.236Z' };
